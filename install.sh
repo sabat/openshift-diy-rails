7fa079b34b98c31d32c2c65698bb14a96fdcdbce
Steve Abatangle <sabat@area51.org>
2015-05-23 07:37:28 -0700

    fix install script

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+RUBY_VER=2.2.2
+RAILS_VER=4.2.1
+
+
+export OPENSHIFT_RUNTIME_DIR=$OPENSHIFT_HOMEDIR/app-root/runtime
 
 cd $OPENSHIFT_TMP_DIR
 
 # get & compile yaml
+rm -vf yaml-0.1.4.tar.gz
 wget http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz
 tar xzf yaml-0.1.4.tar.gz
 cd yaml-0.1.4
@@ -15,9 +22,10 @@ cd $OPENSHIFT_TMP_DIR
 rm -rf yaml*
 
 # get ruby
-wget http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.2.tar.gz
-tar xzf ruby-2.2.2.tar.gz
-cd ruby-2.2.2
+rm -vf ruby-$RUBY_VER.tar.gz
+wget http://cache.ruby-lang.org/pub/ruby/2.2/ruby-$RUBY_VER.tar.gz
+tar xzf ruby-$RUBY_VER.tar.gz
+cd ruby-$RUBY_VER
 
 # export directory with yaml.h
 export C_INCLUDE_PATH=$OPENSHIFT_RUNTIME_DIR/include
@@ -29,12 +37,15 @@ cd ext/psych
 sed -i '1i $LIBPATH << ENV["LIBYAMLPATH"]' extconf.rb
 
 cd $OPENSHIFT_TMP_DIR
-cd ruby-2.2.2
+cd ruby-$RUBY_VER
 
 # compile ruby
 ./configure --disable-install-doc --prefix=$OPENSHIFT_RUNTIME_DIR
 make
 make install
+
+# Fix problem where gem tries to bind to 0.0.0.0 to do DNS lookups (ugh)
+sed --in-place -r -e 's|.0.0.0.0.|ENV["OPENSHIFT_DIY_IP"]|' lib/ruby/2.2.0/resolv.rb
 
 export PATH=$OPENSHIFT_RUNTIME_DIR/bin:$PATH
 
@@ -43,5 +54,5 @@ cd $OPENSHIFT_TMP_DIR
 rm -rf ruby*
 
 # install rails
-gem install rails -v 4.2.1 --no-ri --no-rdoc
+gem install rails --version $RAILS_VER --no-rdoc --no-ri -V
 
